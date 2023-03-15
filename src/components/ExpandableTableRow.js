ace6ba5c92eff27b4d0511bd098100313f7c3bce
Pooja Wagh <tsensuous.grace@gmail.com>
2023-03-14 21:20:00 -0700

    Add files via upload

---
+++ src/components/ExpandableTableRow.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {TableCell, TableRow, IconButton} from '@material-ui/core';
+import {KeyboardArrowDown, KeyboardArrowRight} from '@material-ui/icons';
+import './App.css';
+
+const ExpandableTableRow = ({ children, expandComponent, ...otherProps }) => {
+  const [isExpanded, setIsExpanded] = React.useState(false);
+
+  return (
+    <>
+      <TableRow {...otherProps}>
+        <TableCell padding="checkbox">
+          <IconButton onClick={() => setIsExpanded(!isExpanded)}>
+            {isExpanded ? <KeyboardArrowDown /> : <KeyboardArrowRight />}
+          </IconButton>
+        </TableCell>
+        {children}
+      </TableRow>
+      {isExpanded && (
+        <TableRow>
+          <TableCell padding="checkbox" />
+          {expandComponent}
+        </TableRow>
+      )}
+    </>
+  );
+};
+
+export default ExpandableTableRow;
