d6dafe133014f8cf4e11a1b4f2ea5979b4fbbc97
Pooja Wagh <tsensuous.grace@gmail.com>
2023-03-15 12:46:51 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import {Table, TableBody, TableCell, TableHead, TableRow, Grid,Box ,TableContainer,TablePagination} from "@material-ui/core";
 import ExpandableTableRow from "./components/ExpandableTableRow";
 import PieChart from "./components/Pie";
-import rawFindings from "./store/raw_findings.json";
 import grpFindings from "./store/grouped_findings.json";
 import "./App.css";
 
@@ -41,52 +40,6 @@ function App() {
         return "grey"
     }
   }
-  const getRawFindings = (grpRowId) => {
-    const rowFndingObj = rawFindings.find((item) => item.id=== grpRowId);
-    const severityCls = getSeverityColor(rowFndingObj?.severity);
-    return (rowFndingObj && 
-      <TableRow key={rowFndingObj.id}>
-          <TableCell width="30%">{rowFndingObj.description}</TableCell>
-          <TableCell align="center">
-            <span style= {{background: getBackgroundColor(rowFndingObj.status)}} className="status-cls">{rowFndingObj.status.toUpperCase()}</span>
-          </TableCell>
-          <TableCell align="left">{rowFndingObj.ticket_created}</TableCell>
-          <TableCell align="left">{new Date(rowFndingObj.finding_created).toDateString()}</TableCell>
-          <TableCell align="center" className={`severity-cell  ${severityCls}` }>{rowFndingObj.severity?.toUpperCase()}</TableCell>
-          <TableCell align="left">
-            <div>{rowFndingObj.remediation_text}</div>
-            <div className="remediation-url" onClick={(e) =>{window.open(e.currentTarget.textContent, '_blank');}}>{rowFndingObj.remediation_url}</div>
-          </TableCell>
-      </TableRow>);
-  }
-  const renderExpandedDetailRow = (row) => {
-    return (<Box>
-        <Grid>
-         <TableContainer className="raw-grid">
-            <Table stickyHeader aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell align="center" colSpan={12}>
-                    Raw Findings
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>Description</TableCell>
-                  <TableCell align="center">Status</TableCell>
-                  <TableCell align="center">Created Date</TableCell>
-                  <TableCell align="center">Findings Date</TableCell>
-                  <TableCell align="center">Severity</TableCell>
-                  <TableCell align="center">Remediation</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {getRawFindings(row.id)}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
-      </Box>);
-  }
   return (
     <div className="App">
        <Box>
@@ -97,7 +50,7 @@ function App() {
                       <TableHead>
                         <TableRow>
                             <TableCell padding="checkbox" />
-                            <TableCell>Description</TableCell>
+                            <TableCell>Group Type</TableCell>
                             <TableCell align="left">Status</TableCell>
                             <TableCell align="left">Created Date</TableCell>
                             <TableCell align="left">Severity</TableCell>
@@ -109,8 +62,13 @@ function App() {
                           .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                           .map(row => (
                           <ExpandableTableRow key={row.id}
-                              expandComponent={renderExpandedDetailRow(row)}>
-                            <TableCell align="left">{row.description}</TableCell>
+                              rowId ={row.id}
+                              getBackgroundColor = {getBackgroundColor}
+                              getSeverityColor= {getSeverityColor}>
+                            <TableCell align="left">
+                             <div>{row?.grouping_type?.charAt(0).toUpperCase() + row?.grouping_type.slice(1)}</div>
+                             <div className="remediation-url" onClick={(e) =>{window.open(e.currentTarget.textContent, '_blank')}}>{row.grouping_key}</div>
+                            </TableCell>
                             <TableCell align="center">
                               <span style= {{background: getBackgroundColor(row.status)}} className="status-cls">{row.status.toUpperCase()}</span>
                             </TableCell>
